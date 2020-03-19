0cfa5abd2d1f0869ca413a64e88242167a5411cc
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-19 14:22:00 +1100

    feat: First unit test and url mapping, dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -28,10 +28,24 @@
 # right functionality, and guarantee you never accidentally break it.
 # Unit tests should help you to write code that's clean and bug free.
 
+# So we want to test two things:
+# * Can we resolve the URL for the root of the site("/") to a
+#   particular view function we've made?
+# * Can we make this view function return some HTML which wll get the
+#   functional test to pass?
+
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+    # 1. resolve is the function Django uses internally to resolve
+    #   URLs and find what view function they should map to.
+    # 2. What function is that? It's the view function which will
+    #   actually return the HTML we want.
+    # Every single code change is driven by the tests!
