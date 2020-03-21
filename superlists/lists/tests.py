238587e337b634f2a5acba2a62a80eba8fd88156
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-21 11:58:41 +1100

    docs: Some more TDD philosophy

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -276,3 +276,24 @@ class ListAndItemModelTest(TestCase):
 # are passing but the functional tests aren’t, it’s often pointing at
 # a problem that’s not covered by the unit tests, and in our case,
 # that’s often a template problem.
+
+
+#             Some More TDD Philosophy
+# Working State to Working State (aka The Testing Goat vs.
+# Refactoring Cat)
+#   Our natural urge is often to dive in and fix everything at once...
+#   but if we’re not careful, we’ll end up like Refactoring Cat, in a
+#   situation with loads of changes to our code and nothing working.
+#   The Testing Goat encourages us to take one step at a time, and go
+#   from working state to working state.
+# Split work out into small, achievable tasks
+#   Sometimes this means starting with “boring” work rather than
+#   diving straight in with the fun stuff, but you’ll have to trust
+#   that YOLO-you in the parallel universe is probably having a bad
+#   time, having broken everything, and struggling to get the app
+#   working again.
+# YAGNI
+#   You ain’t gonna need it! Avoid the temptation to write code that
+#   you think might be useful, just because it suggests itself at the
+#   time. Chances are, you won’t use it, or you won’t have
+#   anticipated your future requirements correctly.
