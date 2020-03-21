05d17bd191a3b44a701835100f6cd6bf828dd251
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-22 09:08:38 +1100

    feat: switch to using StaticLiveServerTestCase. --ch08l008--

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -11,7 +11,7 @@
 # to them.
 
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -21,7 +21,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
