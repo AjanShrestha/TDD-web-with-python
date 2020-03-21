2d8c01333cf41d5c37da8eae066ffeb14475a1b8
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-22 08:39:02 +1100

    feat: extend FT to check a second step --ch08l003--

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -143,6 +143,18 @@ def test_layout_and_styling(self):
         delta=10
     )
 
+    # She starts a new list and sees the input is nicely
+    # centered there too
+    inputbox.send_keys('testing')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('1: testing')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+        inputbox.location['x'] + inputbox.size['width'] / 2,
+        512,
+        delta=10
+    )
+
 
 # First functional test (FT)
 # What it's doing
