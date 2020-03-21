c582c12b33b9675044a4664805c8c8213276763a
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-21 11:54:33 +1100

    refactor: Cut down superlists/urls.py. --ch07l045--

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from lists import views
+from django.conf.urls import include, url
+
+from lists import views as list_views
+from lists import urls as list_urls
+# we use the import x as y syntax to alias views and urls. This is
+# good practice in your top-level urls.py, because it will let us
+# import views and urls from multiple apps if we want
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls))
 ]
