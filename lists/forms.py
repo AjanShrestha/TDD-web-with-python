c29528e528a89f1d6d1875e701701814bcab1220
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-23 19:26:09 +1100

    feat: Form now uses modelform

--- lists/forms.py
+++ lists/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 
+from lists.models import Item
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'placeholder': 'Enter a to-do item',
-            'class': 'form-control input-lg'
-        }),
-    )
+
+class ItemForm(forms.models.ModelForm):
+    # ModelForms do all sorts of smart stuff, like assigning sensible
+    # HTML form input types to different types of field, and applying
+    # default validation.
+    class Meta:
+        model = Item
+        fields = ('text',)
+        # In Meta we specify which model the form is for, and which
+        # fields we want it to use.
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg'
+            }),
+        }
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -10,3 +10,18 @@ class ItemFormTest(TestCase):
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
         # form.as_p() renders the form as HTML.
+
+
+# Development-Driven Tests: Using Unit Tests for Exploratory Coding
+# Does this feel a bit like development-driven tests? That’s OK, now
+# and again.
+
+# When you’re exploring a new API, you’re absolutely allowed to mess
+# about with it for a while before you get back to rigorous TDD. You
+# might use the interactive console, or write some exploratory code
+# (but you have to promise the Testing Goat that you’ll throw it away
+# and rewrite it properly later).
+
+# Here we’re actually using a unit test as a way of experimenting
+# with the forms API. It’s actually a pretty good way of learning how
+# it works.
