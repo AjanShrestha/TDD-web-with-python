2be1233e7f237d7252efb745c5da1a07decba1c6
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-23 16:03:50 +1100

    feat: Make a copy of our existing waiter method, change the args. --ch12l010--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -46,6 +46,19 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
 # First functional test (FT)
 # What it's doing
