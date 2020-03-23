0453b1626594b8b08115790d5f39864a97d1ac44
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-23 12:48:26 +1100

    feat: updating settings. --ch11|04--

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
+import string
 
 REPO_URL = 'https://github.com:AjanShrestha/TDD-web-with-python.git'
 
@@ -43,3 +44,32 @@ def _get_latest_source(source_folder):
     # previous version of the code is already there; the equivalent
     # of the git pull we used when we did it manually, but with the
     # reset -- hard to force overwriting any local changes.
+
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path,
+        'ALLOWED_HOSTS =.+$',
+        f'ALLOWED_HOSTS = ["{site_name}"]'
+        )
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    # Django uses SECRET_KEY for some of its crypto—things like
+    # cookies and CSRF protection. It’s good practice to make sure
+    # the secret key on the server is different from the one in your
+    # source code repo, because that code might be visible to
+    # strangers. This section will generate a new key to import into
+    # settings, if there isn’t one there already (once you have a
+    # secret key, it should stay the same between deploys).
+    if not exists(secret_key_file):
+        chars = string.ascii_lowercase + string.digits + '!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, f'SECRET_KEY = {key}')
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+    # append just adds a line to the end of a file. (It’s clever
+    # enough not to bother if the line is already there, but not
+    # clever enough to automatically add a newline if the file
+    # doesn’t end in one. Hence the back-n.)
+    # I’m using a relative import (from .secret_key instead of from
+    # secret_key) to be absolutely sure we’re importing the local
+    # module, rather than one from some‐ where else on sys.path.
