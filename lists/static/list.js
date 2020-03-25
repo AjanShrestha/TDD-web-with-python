70213e9c6865a83b1cdb6ca29100185c4224e4b7
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-25 20:02:41 +1100

    feat: lists.js now exports an initialize function.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,7 @@
-$('input[name="text"]').on('keypress', function() {
-  $('.has-error').hide();
-});
+var initialize = function() {
+  console.log('initalize called');
+  $('input[name="text"]').on('keypress', function() {
+    $('.has-error').hide();
+  });
+};
+console.log('list.js loaded');
