e571de0219db09c9418782c9ae2b58467cfd5faa
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-25 12:31:28 +1100

    feat: use our new dupe item error constant in tests.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -66,9 +66,11 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from django.utils.html import escape
-from unittest import skip
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -218,7 +220,6 @@ class ListViewTest(TestCase):
     # introduce a bug. Helper methods are one of the tools that lower
     # the psychological barrier.
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -227,7 +228,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count, 1)
