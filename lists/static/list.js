cd7cb844dd8238bcc2c73315d3e6d43c2abf9199
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-25 20:03:49 +1100

    feat: call initialize in our tests.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,7 +1,5 @@
 var initialize = function() {
-  console.log('initalize called');
   $('input[name="text"]').on('keypress', function() {
     $('.has-error').hide();
   });
 };
-console.log('list.js loaded');
