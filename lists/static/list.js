c5db9dceda665703e5da2ed3914287958f15fea0
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-25 22:26:28 +1100

    feat: namespace Superlists in lists.js.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,9 @@
-var initialize = function() {
+window.Superlists = {};
+window.Superlists.initialize = function () {
   $('input[name="text"]').on('keypress', function() {
     $('.has-error').hide();
   });
 };
+// Our initialize function name is too generic—what if we include 
+// some third-party JavaScript tool later that also defines a 
+// function called initialize? 
