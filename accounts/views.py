6a1c73db3dac6e44fb5ab1b7293b2af6265036f6
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-26 12:28:13 +1100

    feat: SPIKE logout view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 import uuid
 import sys
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 
@@ -30,3 +30,8 @@ def login(request):
     if user is not None:
         auth_login(request, user)
     return redirect("/")
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
