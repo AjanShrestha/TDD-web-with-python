7272f4127e596fc9880c0461a5b6c8c1ebe4e96d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-26 12:17:37 +1100

    feat: SPIKE: user model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
 
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(max_length=255)
+
+
+class ListUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(primary_key=True)
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['email', 'height']
+
+    objects = ListUserManager()
+
+    @property
+    def is_staff(self):
+        return self.email == 'ajan.shrestha@example.com'
+
+    @property
+    def is_active(self):
+        return True
