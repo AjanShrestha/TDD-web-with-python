21de4ffd6cfc145f51c0bfa2d6e9da12fe4e83e2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-27 11:30:25 +1100

    feat: Use token in url in message body.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.contrib import messages
 from django.shortcuts import redirect
 
@@ -8,9 +9,18 @@ from accounts.models import Token
 def send_login_email(request):
     email = request.POST['email']
     token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        f"{reverse('login')}?token={str(token.uid)}"
+    )
+    # request.build_absolute_uri deserves a mention—it’s one way to
+    # build a “full” URL, including the domain name and the http(s)
+    # part, in Django. There are other ways, but they usually involve
+    # getting into the “sites” framework, and that gets
+    # overcomplicated pretty quickly.
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'Use this link to log in',
+        message_body,
         'noreply@superlists',
         [email],
     )
