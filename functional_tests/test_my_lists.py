7ac21402e11dde5e8364c9d6f1e75b584e7c90d4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-03-29 08:07:40 +1100

    docs: JSON test Fixtures Considered Harmful

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -39,3 +39,32 @@ class MyListsTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_in(email)
+
+
+#       JSON test Fixtures Considered Harmful
+
+# When we pre-populate the database with test data, as we’ve done
+# here with the User object and its associated Session object, what
+# we’re doing is setting up a “test fixture”.
+
+# Django comes with built-in support for saving database objects as
+# JSON (using the manage.py dumpdata), and automatically loading them
+# in your test runs using the fixtures class attribute on TestCase.
+
+# More and more people are starting to say: don’t use JSON fixtures.
+# They’re a nightmare to maintain when your model changes. Plus it’s
+# difficult for the reader to tell which of the many attribute values
+# specified in the JSON are critical for the behaviour under test,
+# and which are just filler. Finally, even if tests start out sharing
+# fixtures, sooner or later one test will want slightly different
+# versions of the data, and you end up copying the whole thing around
+# to keep them isolated, and again it’s hard to tell what’s relevant
+# to the test and what is just happenstance.
+
+# It’s usually much more straightforward to just load the data
+# directly using the Django ORM.
+
+# Once you have more than a handful of fields on a model, and/or
+# several related models, even using the ORM can be cumbersome. In
+# this case, there’s a tool that lots of people swear by called
+# factory_boy.
