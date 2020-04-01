fd639b7d9a35f21d37ba6ed773a5561f829c7593
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-04-01 16:08:02 +1100

    docs: Hiding ORM Code Behind Helper Methods

--- lists/forms.py
+++ lists/forms.py
@@ -60,3 +60,37 @@ class ExistingListItemForm(ItemForm):
 
     def save(self):
         return forms.models.ModelForm.save(self)
+
+
+#           Hiding ORM Code Behind Helper Methods
+# One of the techniques that emerged from our use of isolated tests
+# was the “ORM helper method”.
+
+# Django’s ORM lets you get things done quickly with a reasonably
+# readable syntax (it’s certainly much nicer than raw SQL!). But some
+# people like to try to minimise the amount of ORM code in the
+# application—particularly removing it from the views and forms
+# layers.
+
+# One reason is that it makes it much easier to test those layers.
+# But another is that it forces us to build helper functions that
+# express our domain logic more clearly. Compare:
+        # list_ = List()
+        # list_.save()
+        # item = Item()
+        # item.list = list_
+        # item.text = self.cleaned_data['text']
+        # item.save()
+# With:
+        # List.create_new(first_item_text=self.cleaned_data['text']
+# This applies to read queries as well as write. Imagine something
+# like this:
+        # Book.objects.filter(in_print=True, pub_date__lte=datetime.
+        # today())
+# Versus a helper method, like:
+        # Book.all_available_books()
+# When we build helper functions, we can give them names that express
+# what we are doing in terms of the business domain, which can
+# actually make our code more legi‐ ble, as well as giving us the
+# benefit of keeping all ORM calls at the model layer, and thus
+# making our whole application more loosely coupled.
