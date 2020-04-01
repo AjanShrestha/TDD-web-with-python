b0fe7946e8118cfe16c050c68e88932499e7d9d2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-04-01 16:35:05 +1100

    feat: flesh out test that create_new should return list.

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -138,7 +138,9 @@ class ListModelTest(TestCase):
         self.assertEqual(new_list.owner, user)
 
     def test_create_returns_new_list_object(self):
-        self.fail()
+        returned = List.create_new(first_item_text='new item text')
+        new_list = List.objects.first()
+        self.assertEqual(returned, new_list)
 
     def test_lists_can_have_owners(self):
         List(owner=User())  # should not raise
