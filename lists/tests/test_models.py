c6d3384c4173f722dd94cab7fd475b7576a1e7c2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-04-01 16:41:37 +1100

    feat: test list.name is first item text

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -124,6 +124,12 @@ class ListModelTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
+    def test_list_name_is_first_item_text(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='first item')
+        Item.objects.create(list=list_, text='second item')
+        self.assertEqual((list_.name, 'first item'))
+
     def test_create_new_creates_list_and_first_item(self):
         List.create_new(first_item_text='new item text')
         new_item = Item.objects.first()
