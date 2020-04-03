d897fe1a6aa2f1f12431f5260281bae958e6a21a
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-04-03 15:55:12 +1100

    feat: implement shared_with at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -148,6 +148,13 @@ class ListModelTest(TestCase):
         new_list = List.objects.first()
         self.assertEqual(returned, new_list)
 
+    def test_can_share_with_another_user(self):
+        list_ = List.objects.create()
+        user = User.objects.create(email='a@b.com')
+        list_.shared_with.add('a@b.com')
+        list_in_db = List.objects.get(id=list_.id)
+        self.assertIn(user, list_in_db.shared_with.all())
+
     def test_lists_can_have_owners(self):
         List(owner=User())  # should not raise
 
