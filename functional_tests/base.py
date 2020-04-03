4c5bf07a41f8a27b0dc00db8d2bac20e7d36cec3
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-04-03 13:11:09 +1100

    refactor: move create pre auth'd session into base.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -11,6 +11,7 @@
 # to them.
 
 
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from datetime import datetime
 from selenium import webdriver
@@ -19,7 +20,11 @@ from selenium.webdriver.common.keys import Keys
 import os
 import time
 
-from .server_tools import reset_database
+from .management.commands.create_session import create_pre_authenticated_session
+from .server_tools import (
+    create_session_on_server,
+    reset_database
+)
 
 
 SCREEN_DUMP_LOCATION = os.path.join(
@@ -142,6 +147,27 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def create_pre_authenticated_session(self, email):
+        # 1
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,  # 2
+            path='/',
+        ))
+        # 1. We create a session object in the database. The session
+        #   key is the primary key of the user object (which is
+        #   actually the user’s email address).
+        # 2. We then add a cookie to the browser that matches the
+        #   session on the server—on our next visit to the site, the
+        #   server should recognise us as a logged-in user.
 
 
 # First functional test (FT)
